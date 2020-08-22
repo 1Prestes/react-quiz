7f72d5280764d24d4fd4f3ab0b09a55a8dd264c8
Dijango Alves <dijango_ilha@hotmail.com>
2020-08-22 13:46:40 -0300

    feat: add API to get questions

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
 
-const QuestionCard = () => <div>Question Card</div>;
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNumber: number;
+  totalQuestion: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNumber,
+  totalQuestion,
+}) => (
+  <div>
+    <p className="number">
+      Question: {questionNumber} / {totalQuestion}
+    </p>
+    <p dangerouslySetInnerHTML={{__html: question}} />
+    <div>
+      {answers.map((answer) => (
+        <div>
+          <button disabled={userAnswer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{__html: answer}} />
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 export default QuestionCard;
